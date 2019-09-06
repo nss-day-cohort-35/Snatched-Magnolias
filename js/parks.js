98650f97b75489a3f8c7e26e8bd879916e4f5439
sethoak <sethoak@gmail.com>
2019-09-05 21:05:46 -0500

    updated parks.js

--- js/parks.js
+++ js/parks.js
@@ -2,5 +2,5 @@ fetch("https://data.nashville.gov/resource/74d7-b74t.json")
   .then(response => response.json())
   .then(parsedData => {
     console.table(parsedData);
-    createNasaContainer(createNasa(parsedData));
+    return parsedData.events;
   });
