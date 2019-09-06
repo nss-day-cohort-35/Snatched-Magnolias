e4d9b6027771ba491e4e6e97e58073429391167d
sethoak <sethoak@gmail.com>
2019-09-06 13:27:36 -0500

    updated parks.js

--- js/parks.js
+++ js/parks.js
@@ -1,22 +1,24 @@
-// Cotton Eyed Joe AND HE GIVES IT BACK as a string
-function dataDisplay(data) {
-  return `<h3>${data.name.text}<h3>`;
-}
-
-// Gives 1 out of 50 balls to Cotton Eyed Joe
 function soResults(array) {
   array.forEach(data => {
     document.querySelector("#section-results").innerHTML += dataDisplay(data);
   });
 }
 
-function lookUp (searchTerm){
+function lookUp(searchTerm) {
   fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+  .then(result => result.json())
+  .then(parsedResult => {
+      console.log(parsedResult.restaurants);
+      parsedResult.restaurants.forEach(element => {
+          document.querySelector("#section-results").innerHTML += dataDisplay (data);
+      });
 
+  });
+}
 
-
-
-document.querySelector("#search-meetUps").addEventListener ("click", event => {
-  let searchTerm = document.querySelector("#so-search-bar").value
+document.querySelector("#search-parks").addEventListener("click", event => {
+  let searchTerm = document.querySelector("#so-search-bar").value;
   lookUp(searchTerm);
 });
+
+lookUp();
