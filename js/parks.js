81a9a8afbd3a6f1ef30f419788d1710a8248562f
sethoak <sethoak@gmail.com>
2019-09-07 12:31:39 -0500

    updated parks.js w/more results for parks

--- js/parks.js
+++ js/parks.js
@@ -5,12 +5,19 @@ function lookUp(searchTerm) {
       parsedData.forEach(element => {
         console.log(element.park_name);
         console.log(element.acres);
+        console.log(element.disc_golf);
         document.querySelector(
           "#section-results"
-        ).innerHTML += `<h3>${element.park_name}</h3>`;
+        ).innerHTML += `<h3>Park Name: ${element.park_name}`;
         document.querySelector(
           "#section-results"
-        ).innerHTML += `<h3>${element.acres}</h3>`;
+        ).innerHTML += `<h3>Disc Golf: ${element.disc_golf}</h3>`;
+        document.querySelector(
+          "#section-results"
+        ).innerHTML += `<h3>Dog Park: ${element.dog_park}</h3>`;
+        document.querySelector(
+          "#section-results"
+        ).innerHTML += `<h3>Hiking Trails: ${element.hiking_trails}</h3>`;
       });
     });
 }
@@ -19,3 +26,29 @@ document.querySelector("#search-parks").addEventListener("click", event => {
   let searchTerm = document.querySelector("#so-search-bar").value;
   lookUp(searchTerm);
 });
+
+if ("geolocation" in navigator) {
+  // check if geolocation is supported/enabled on current browser
+  navigator.geolocation.getCurrentPosition(
+    function success(position) {
+      // for when getting location is a success
+      console.log(
+        "latitude",
+        position.coords.latitude,
+        "longitude",
+        position.coords.longitude
+      );
+    },
+    function error(error_message) {
+      // for when getting location results in an error
+      console.error(
+        "An error has occured while retrieving location",
+        error_message
+      );
+    }
+  );
+} else {
+  // geolocation is not supported
+  // get your location some other way
+  console.log("geolocation is not enabled on this browser");
+}
