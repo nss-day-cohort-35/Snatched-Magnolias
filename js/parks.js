05fcca16beb8dd0b7b80249899292ef442e6cb03
sethoak <sethoak@gmail.com>
2019-09-09 14:58:37 -0500

    updated parks.js

--- js/parks.js
+++ js/parks.js
@@ -1,3 +1,4 @@
+/*
 function soLookUp(searchTerm) {
   fetch("https://data.nashville.gov/resource/74d7-b74t.json")
     .then(response => response.json())
@@ -21,7 +22,39 @@ function soLookUp(searchTerm) {
       });
     });
 }
+*/
 
+function soLookUp(searchTerm) {
+  return fetch("https://data.nashville.gov/resource/74d7-b74t.json")
+    .then(response => response.json())
+    .then(parsedResult => {
+      return parsedResult;
+    });
+}
+
+document.querySelector("#search-parks").addEventListener("click", event => {
+  let searchTerm = document.querySelector("#so-search-bar").value;
+  soLookUp(searchTerm).then(parkData => {
+    console.log(parkData);
+    parkData.forEach(item => {
+      document.querySelector(
+        "#section-results"
+      ).innerHTML += `<h3 id=${item.park_name}>${item.park_name}</h3> <button type="button" id="buttonIdso--${item.park_name}">ADD</button>`;
+    });
+  });
+});
+
+document.querySelector("#section-results").addEventListener("click", event => {
+  if (event.target.id.startsWith("buttonIdso")) {
+    let itemId = event.target.id.split("--")[1];
+    let selectedItem = document.getElementById(itemId);
+    let itineraryContainer = document.getElementById("parkItinerary");
+    itineraryContainer.appendChild(selectedItem);
+    document.querySelector("#section-results").innerHTML = " ";
+  }
+});
+
+/*
 document.querySelector("#search-parks").addEventListener("click", event => {
   let searchTerm = document.querySelector("#so-search-bar").value;
   soLookUp(searchTerm);
@@ -36,3 +69,4 @@ document.querySelector("#section-results").addEventListener("click", event => {
     document.querySelector("#section-results").innerHTML = "";
   }
 });
+*/
