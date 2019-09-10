f8e42c9d68fc23c4297550d75e50bf4e854ed248
Sage Klein <sageklein@gmail.com>
2019-09-09 19:52:35 -0500

    added an ADD button to displayed data.

--- js/meetUps.js
+++ js/meetUps.js
@@ -1,29 +1,46 @@
 // Cotton Eyed Joe AND HE GIVES IT BACK as a string
-function dataDisplay (data) {
-return `<h3>${data.name.text}<h3>`
+function dataDisplay(text, index) {
+  return `<h3>${text}<h3> <button type="button" onclick="addMeetUpsToItin(${index})">ADD</button>`;
 }
 
-// Gives 1 out of 50 balls to Cotton Eyed Joe
-function skResults (array) {
-array.forEach(data => {
-document.querySelector("#section-results").innerHTML += dataDisplay(data);
-});
+function addMeetUpsToItin (meetUpIndex){
+    let meetUpName = meetUpsArray[meetUpIndex].name.text;
 }
 
-function lookUp (searchTerm){
-  fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchTerm}&location.address=nashville&token=7JJ7JBOQ23XZHTFLOROV`)      
+
+//  Searching the API for obj.name
+let meetUpsArray = [];
+
+function skMeetUps(searchTerm) {
+  fetch(
+    `https://www.eventbriteapi.com/v3/events/search/?q=${searchTerm}&location.address=nashville&token=7JJ7JBOQ23XZHTFLOROV`
+  )
     .then(result => result.json())
-        .then(parsedResult => {
-            console.log(parsedResult.meetUps);
-            parsedResult.meetUps.forEach(element => {
-                document.querySelector("#section-results").innerHTML += dataDisplay (data);
-            });
-    
-        });
+    .then(parsedResult => {
+      let contentBox = document.querySelector("#section-results");
+      contentBox.innerHTML = "";
+      meetUpsArray = [];
+      parsedResult.events.forEach((event, index) => {
+        contentBox.innerHTML += dataDisplay(event.name.text, index);
+        meetUpsArray[index] = event;
+      });
+    });
 }
 
-document.querySelector("#search-meetUps").addEventListener ("click", event => {
-    let searchTerm = document.querySelector("#sk-search-bar").value
-    lookUp(searchTerm)
-});
+//Event Listener for search click
+document.querySelector("#search-meetUps")
+  .addEventListener("click", event => {
+    let searchTerm = document.querySelector("#sk-search-bar").value;
+    skMeetUps(searchTerm);
+  });
 
+document.querySelector("#section-results").addEventListener("click", event => {
+  if (event.target.id.startsWith("buttonIdsk")) {
+    let itemId = event.target.id.split("--")[1];
+    let selectedItem = document.getElementById(itemId);
+    let itineraryContainer = document.getElementById("meetUpsItinerary");
+    itineraryContainer.innerHTML = "";
+    itineraryContainer.appendChild(selectedItem);
+    document.querySelector("#section-results").innerHTML = " ";
+  }
+});
